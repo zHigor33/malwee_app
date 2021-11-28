2834c75a7af5f419565daf3bc57871e5815ef41a
zHigor33 <higor_pinho@estudante.sc.senai.br>
2021-11-28 18:39:55 -0300

    schema alterado

--- parque_malwee_db.sql
+++ parque_malwee_db.sql
@@ -139,3 +139,82 @@ SELECT * FROM `user`;
 -- DROP DATABASE
 -- -----------------------------------------------------
 drop database parque_malwee;
+
+
+
+
+
+CREATE SCHEMA IF NOT EXISTS `parque_malwee` DEFAULT CHARACTER SET utf8 ;
+
+USE `parque_malwee`;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`user` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(300) NOT NULL,
+  `user_password` VARCHAR(500) NOT NULL,
+  `user_name` VARCHAR(200) NOT NULL,
+  `user_adm` TINYINT NOT NULL DEFAULT 0,
+  `number_phone` CHAR(11) NULL,
+  `profile_image` VARCHAR(1000) NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `iduser_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
+  UNIQUE INDEX `number_phone_UNIQUE` (`number_phone` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`waypoint` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `lat` VARCHAR(100) NOT NULL,
+  `log` VARCHAR(100) NOT NULL,
+  `local_name` VARCHAR(100) NOT NULL,
+  `local_image` VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `idwaypoint_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `local_name_UNIQUE` (`local_name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`event` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `event_name` VARCHAR(100) NOT NULL,
+  `description` VARCHAR(500) NOT NULL,
+  `event_date` DATE NOT NULL,
+  `event_time` TIME NOT NULL,
+  `event_image` VARCHAR(1000) NOT NULL,
+  `waypoint_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `waypoint_ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  INDEX `fk_event_waypoint1_idx` (`waypoint_ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`user_event` (
+  `user_ID` INT NOT NULL,
+  `event_ID` INT NOT NULL,
+  PRIMARY KEY (`user_ID`, `event_ID`),
+  INDEX `fk_user_has_event_event1_idx` (`event_ID` ASC) VISIBLE,
+  INDEX `fk_user_has_event_user_idx` (`user_ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`activity` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `activity_name` VARCHAR(80) NOT NULL,
+  `activity_description` VARCHAR(500) NOT NULL,
+  `image` VARCHAR(1000) NOT NULL,
+  `waypoint_ID` INT NOT NULL,
+  PRIMARY KEY (`ID`, `waypoint_ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  INDEX `fk_activity_waypoint1_idx` (`waypoint_ID` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `parque_malwee`.`museum` (
+  `ID` INT NOT NULL AUTO_INCREMENT,
+  `artifact_name` VARCHAR(80) NOT NULL,
+  `artifact_description` VARCHAR(500) NOT NULL,
+  `image` VARCHAR(1000) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `ID_UNIQUE` (`ID` ASC) VISIBLE,
+  UNIQUE INDEX `artifact_name_UNIQUE` (`artifact_name` ASC) VISIBLE)
+ENGINE = InnoDB;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
